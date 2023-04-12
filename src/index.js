721d0ecbe9a19d8726d4aeec5ea2a43e94897c27
Mutindi Hope <msndindi@outlook.com>
2023-04-12 09:08:03 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.querySelector("#new-task-description");
+  const submitButton = document.querySelector("#submitButton");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addTask();
+  });
+
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addTask();
+  });
+
+  function addTask() {
+    const taskList = document.querySelector("#tasks");
+    const task = taskInput.value;
+    const taskItem = document.createElement("li");
+    taskItem.innerText = task;
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  }
 });
